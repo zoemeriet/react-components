3a34637e1e281b90b805031b336c5cd43acbb476
zoemeriet <zoe@binch-solutions.com>
2023-12-06 10:12:14 +0100

    feat: add style

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,4 @@
+import './index.css';
+
 export * from './card';
 export * from './button';
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig ({
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.tsx"),
-      name: "react-components",
-   		 
+      name: "react-components", 
       fileName: "index",
     },
     rollupOptions: {
-      external: ["react"],
+      external: ["react", "tailwindcss"],
+      output: {
+        globals: {
+          react: 'React',
+          tailwindcss: 'tailwindcss',
+        }
+      }
     },
   },
 });
