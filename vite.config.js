493a34a3c51da6ae7190669704a4910e4d56b454
zoemeriet <zoe@binch-solutions.com>
2023-12-06 10:27:33 +0100

    feat: add css

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import tailwindcss from 'tailwindcss';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 export default defineConfig ({
+  plugins: [
+    libInjectCss(),
+  ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.tsx"),
